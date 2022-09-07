03f0aa32992c7e1105a2ca29a88cc659ab767a76
Sarah <48202579+SarahWaest@users.noreply.github.com>
2022-09-07 21:02:05 +0200

    Add files via upload

---
+++ WhatToDo-master/Backend/WhatToDo/RestAPI/Models/InMemoryDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestApi.Controllers;
+
+namespace RestApi.Models
+{
+    public class InMemoryDbContext : DbContext
+    {
+        public InMemoryDbContext(DbContextOptions<InMemoryDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Activity> ActivityItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<SensorData> Sensordatas { get; set; }
+    }
+}
